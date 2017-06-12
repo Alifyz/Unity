2873d47bcaa2b6548dc2f63d7e3d10688c610dfa
Alifyz Pires <alifyz@outlook.com>
2017-06-12 20:58:06 -0300

    Adding Unity3D Gligh Garden Project

---
+++ Glith/Glitch/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor.SceneManagement;
+
+public class LevelManager : MonoBehaviour {
+
+    public float LoadNextLevelTime;
+
+	void Start () {
+        Invoke("LoadNextLevel", LoadNextLevelTime);
+	}
+
+
+    public void LoadNextLevel()
+    {
+        Application.LoadLevel(Application.loadedLevel + 1);
+        
+    }
+
+}
