af627a8da1eaa91f39d327498d190202fa3591f5
Alifyz Pires <alifyz@outlook.com>
2017-06-13 00:45:43 -0300

    New Audio System Remastered

---
+++ Glith/Glitch/Assets/Scripts/MusicManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MusicManager : MonoBehaviour {
+
+    //Audio System Remastered
+    public AudioClip[] audioChanger;
+    private AudioSource audioSource;
+
+    //Make the GameObject Persist
+	void Awake () {
+        DontDestroyOnLoad(gameObject);
+	}
+    //Initializating the new Gameobjects
+    void Start() {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    private void OnLevelWasLoaded(int level)
+    {
+        
+    }
+
