e20a1df70af0a9df41c026b618ea08518146ff1d
Alifyz Pires <alifyz@outlook.com>
2017-06-19 00:11:01 -0300

    Implementing, OnLevelWasLoaded Delegate

--- Glith/Glitch/Assets/Scripts/MusicManager.cs
+++ Glith/Glitch/Assets/Scripts/MusicManager.cs
@@ -3,23 +3,46 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class MusicManager : MonoBehaviour {
+public class MusicManager : MonoBehaviour
+{
 
     //Audio System Remastered
     public AudioClip[] audioChanger;
     private AudioSource audioSource;
 
-    //Make the GameObject Persist
-	void Awake () {
-        DontDestroyOnLoad(gameObject);
-	}
-    //Initializating the new Gameobjects
-    void Start() {
-        audioSource = GetComponent<AudioSource>();
-    }
-    //Changing the Music
-    private void OnLevelWasLoaded(int level)
+    private void OnEnable()
     {
-        
+        SceneManager.sceneLoaded += OnSceneLoaded;
     }
 
+    private void OnDisable()
+    {
+        SceneManager.sceneLoaded -= OnSceneLoaded;
+    }
+
+    //Make the GameObject Persist
+    void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+    //Initializating the new Gameobjects
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        AudioClip thisLevelMusic = audioChanger[scene.buildIndex];
+        if(thisLevelMusic)
+        {
+            audioSource.clip = thisLevelMusic;
+            audioSource.loop = true;
+            audioSource.Play();
+        }
+    }
+
+    
+    
+
+}
