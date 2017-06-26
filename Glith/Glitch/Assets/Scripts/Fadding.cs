d5f8f5ac9ba97d98df2531748a33a5e486903549
Alifyz Pires <alifyz@outlook.com>
2017-06-26 00:45:42 -0300

    Fade in Effect Glith Project

---
+++ Glith/Glitch/Assets/Scripts/Fadding.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Fadding : MonoBehaviour {
+
+   
+    public float faddingTime = 2.0f;
+    private Image fadeTexture;
+    private Color currentColor = Color.black;
+
+    void Start()
+    {
+        fadeTexture = GetComponent<Image>();
+    }
+
+
+    //Transition Effect FadeIN Login
+    //Changin the Alpha Channel.
+     void Update()
+    {
+        if (Time.timeSinceLevelLoad < faddingTime)
+            {
+                float alphaChange = Time.deltaTime / faddingTime;
+                currentColor.a -= alphaChange;
+                fadeTexture.color = currentColor;
+            }
+
+        else
+            {
+                gameObject.SetActive(false);
+            }
+    }
+
+
+
+
+
+}
